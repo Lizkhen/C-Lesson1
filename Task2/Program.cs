1ccf7d04c6cfeec9acd38292e75ccec7b3fd0825
Lizkhen <slezadrakona@gmail.com>
2022-12-02 15:14:54 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{   
+    Console.WriteLine (number1 + " больше, "+ number2 + " меньше");
+}
+else 
+    Console.WriteLine (number2 + " больше, "+ number1 + " меньше"); 
