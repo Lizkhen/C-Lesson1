6bf8c131890aa6ab6b8e7f2f63786be6ca38475d
Lizkhen <slezadrakona@gmail.com>
2022-12-02 18:57:46 +0300

    Task2 new

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,5 +4,12 @@ if (number1>number2)
 {   
     Console.WriteLine (number1 + " больше, "+ number2 + " меньше");
 }
-else 
+if (number1==number2)
+{
+     Console.WriteLine (number1 + " и " + number2 + " равны");
+}
+if (number1<number2)
+{ 
     Console.WriteLine (number2 + " больше, "+ number1 + " меньше"); 
+}
+   
