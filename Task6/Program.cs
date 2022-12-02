bdad85ef4726a24dff3067694e18549fc400a331
Lizkhen <slezadrakona@gmail.com>
2022-12-02 18:09:41 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+if (number%2==0)
+{
+    Console.WriteLine ("Число "+ number+ " чётное");
+}
+else 
+    Console.WriteLine ("Число "+ number+ " нечётное");
