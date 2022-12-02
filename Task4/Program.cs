42ea9e2f61ca037e8138657ae243ebdbf1544843
Lizkhen <slezadrakona@gmail.com>
2022-12-02 17:21:48 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2>max)
+{
+    max = number2;  
+}
+else 
+    max = number1;        
+ if (number3>max)
+{
+    max = number3;
+}  
+Console.WriteLine (max);
